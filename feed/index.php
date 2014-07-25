a3f0a94d4fb1183e4188fe5aa88d1704c76024a0
The Metaist <metaist@metaist.com>
2014-07-25 16:38:43 -0400

    update: feed self tag; lead text

--- feed/index.php
+++ feed/index.php
@@ -5,7 +5,7 @@
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
   <title>Metaist</title>
-  <link rel="self" href="//metaist.com/show-me/progress" />
+  <link rel="self" href="//metaist.com/show-me-progress/feed" />
   <updated><?php echo $dt?>T00:00:00+00:00</updated>
   <author>
     <name>The Metaist</name>
@@ -13,7 +13,7 @@
   <id>tag:metaist.com,2014:show-me.progress</id>
   <entry>
     <title>Show Me Progress: <?php echo $dt?></title>
-    <link href="//metaist.com/show-me/progress" />
+    <link href="//metaist.com/show-me-progress" />
     <link rel="alternate" type="text/html" href="//metaist.com/show-me/progress"/>
     <id>tag:metaist.com,2014:show-me.progress-<?php echo sha1($dt); ?></id>
     <published><?php echo $dt?>T00:00:00+00:00</published>
@@ -21,7 +21,7 @@
     <author>
       <name>The Metaist</name>
     </author>
-    <content type="html">&lt;p&gt;Take 5 minutes to respond to these questions:&lt;/p&gt;&lt;ol&gt;
+    <content type="html">&lt;p&gt;Click &quot;Reply&quot; and answer these questions about your goal:&lt;/p&gt;&lt;ol&gt;
   &lt;li&gt;What did you do yesterday?&lt;br /&gt;&lt;br /&gt;&lt;/li&gt;
   &lt;li&gt;What will you do today?&lt;br /&gt;&lt;br /&gt;&lt;/li&gt;
   &lt;li&gt;What are obstacles to being done on-time?&lt;br /&gt;&lt;br /&gt;&lt;/li&gt;
